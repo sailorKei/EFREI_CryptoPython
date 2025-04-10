b21e33f438d240cef08039f3e926df45364fed70
sailorKei <kloranelabonne@outlook.fr>
2025-04-10 17:48:01 +0200

    Ajout de hello.html dans templates

--- __init__.py
+++ __init__.py
@@ -1,24 +1,12 @@
-from cryptography.fernet import Fernet
-from flask import Flask, render_template_string, render_template, jsonify
+from flask import Flask
 from flask import render_template
-from flask import json
-from urllib.request import urlopen
-import sqlite3
-                                                                                                                                       
-app = Flask(__name__)                                                                                                                  
-                                                                                                                                       
+
+app = Flask(__name__)
+
 @app.route('/')
 def hello_world():
     return render_template('hello.html')
 
-key = Fernet.generate_key()
-f = Fernet(key)
-
-@app.route('/encrypt/<string:valeur>')
-def encryptage(valeur):
-    valeur_bytes = valeur.encode()  # Conversion str -> bytes
-    token = f.encrypt(valeur_bytes)  # Encrypt la valeur
-    return f"Valeur encryptée : {token.decode()}"  # Retourne le token en str
-                                                                                                                                                     
 if __name__ == "__main__":
   app.run(debug=True)
+
